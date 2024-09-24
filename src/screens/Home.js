6db9ffebf755d3480e27fadf9a948520ae3eb94c
JacksonLira <94633582+JacksonLira@users.noreply.github.com>
2024-09-23 21:51:31 -0300

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -66,6 +66,29 @@ function Home() {
     arrows:true,
     autoplay:true,
     autoplayspeed: 300,
+     responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
 
     
   }
