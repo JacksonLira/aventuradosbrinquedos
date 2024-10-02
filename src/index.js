9122f73c58d1305d92eaa075720b26a58acab9d4
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-10-02 03:43:34 -0400

    feat: replacing npm package manager with yarn - now use: yarn start to start the project - adding .gitignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Routes from './Routes/Routes'
-
+import Routes from './routes/routes'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
