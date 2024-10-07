1ebe6282050fd55ad9b2229e349dc2e1b15a4f96
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-10-07 16:49:09 -0400

    fix: ajustes de propriedades html

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import Routes from './Routes/Routes'
+import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Routes />
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
   </React.StrictMode>
 )
